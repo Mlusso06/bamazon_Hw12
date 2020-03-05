cd55fe0ad449a06d56cad5f74c58a538ba4b7d1f
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-03-04 22:56:14 -0600

    correcting syntx on the Database

--- bamazon.sql
+++ bamazon.sql
@@ -12,5 +12,5 @@ CREATE TABLE products (
   PRIMARY KEY (id)
 );
 
-INSERT INTO produts (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Hefty Strong Trash Bag", "HouseHold", 11.99, 50), ("GearLight LED FlashLight", "Safety", 19.99, 20), ("TAG Heuer Men's Watch", "Fashion", 250.00, 10), ("Apple Iphone10 Case", "CP Accessories", 15.00, 45), ("HP Blak Ink Cartrige", "office", 20.00, 15), ("Biginner's Guide to Coding", "Books", 11.99, 4), ("Golf Shoes", "Athletics", 44.99, 6);
